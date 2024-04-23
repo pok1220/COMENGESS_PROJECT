6fb305328bf18d6245facff75fdd5d00afc637a7
pok1220 <pokkung1220@gmail.com>
2024-04-23 19:03:24 +0700

    add aws IP

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,5 +1,5 @@
 // TODO0: change the BACKEND_URL to your EC2 public IPv4
-export const BACKEND_URL = "http://localhost:3222";
+export const BACKEND_URL = "http://3.220.226.106:3222";
 
 // TODO0: change element in MEMBERS to your group members.
 // WARNING: the _id parameters is required.
